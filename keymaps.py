2109a6327baa1801f9d95ffa4a1725f6848a86f4
Quackers <shadowrogue624@gmail.com>
2024-08-07 15:58:41 +0800

    Prefs: Cleanup and organize preferences menu

--- keymaps.py
+++ keymaps.py
@@ -5,17 +5,11 @@ from .operators import (
 
 
 keymap_info = {"keymap_name" : "Node Editor", "space_type" : "NODE_EDITOR",}
-keymap_info_2 = {"keymap_name" : "User Interface", "space_type" : "EMPTY",}
 
 
-keymap_structure = KeymapStructure(
-    {
-    "Group": (
-        ),
-    "Active Nodegroup": (
-        KeymapItemDef(GROUP_TOOLS_OT_copy_from_active.bl_idname, **keymap_info),
-        ),
-    }
+keymap_structure = KeymapStructure([
+    KeymapItemDef(GROUP_TOOLS_OT_copy_from_active.bl_idname, **keymap_info),
+    ]
 )
 
 keymap_layout = KeymapLayout(layout_structure=keymap_structure)
