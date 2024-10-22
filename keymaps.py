a9da34ceaa8437019cf91b4683cbad4b15125b15
Quackers <shadowrogue624@gmail.com>
2024-10-22 11:11:30 +0800

    Keymaps: Add hotkey for "Set Default Width" operator

--- keymaps.py
+++ keymaps.py
@@ -1,16 +1,26 @@
+import bpy
+
 from .keymap_ui import KeymapItemDef, KeymapStructure, KeymapLayout
 from .operators import (
     GROUP_TOOLS_OT_copy_from_active,
+    GROUP_TOOLS_OT_selected_group_default_width_set,
+
 )
 
 
 keymap_info = {"keymap_name" : "Node Editor", "space_type" : "NODE_EDITOR",}
 
-
-keymap_structure = KeymapStructure([
-    KeymapItemDef(GROUP_TOOLS_OT_copy_from_active.bl_idname, **keymap_info),
-    ]
-)
+if bpy.app.version >= (4, 3, 0):
+    keymap_structure = KeymapStructure([
+        KeymapItemDef(GROUP_TOOLS_OT_copy_from_active.bl_idname, **keymap_info),
+        KeymapItemDef(GROUP_TOOLS_OT_selected_group_default_width_set.bl_idname, **keymap_info),
+        ]
+    )
+else:
+    keymap_structure = KeymapStructure([
+        KeymapItemDef(GROUP_TOOLS_OT_copy_from_active.bl_idname, **keymap_info),
+        ]
+    )
 
 keymap_layout = KeymapLayout(layout_structure=keymap_structure)
 
